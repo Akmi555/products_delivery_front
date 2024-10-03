bd19effc7a43119eee6953cc27c6d49b92e3eb71
“Elvira55872” <elli-x1996@mail.ru>
2024-10-03 10:20:02 +0200

    installed npm install @mui/icons-material

--- src/components/ProductButton/styles.ts
+++ src/components/ProductButton/styles.ts
@@ -2,13 +2,12 @@ import styled from "@emotion/styled"
 import { ButtonStyledProps } from "./types"
 
 export const ButtonStyled = styled("button")<ButtonStyledProps>`
-display: flex;
-padding: 0;
+  display: flex;
+  padding: 0;
   width: 100%;
   cursor: pointer;
   border: none;
   background-color: white;
-
 `
 
 export const ButtonImg = styled.img`
