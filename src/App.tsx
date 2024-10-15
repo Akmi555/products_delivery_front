89c71aa7cee4469abe2dfc69e787f557fc219c4e
Dimitri Diel <150340219+DimitriDiel@users.noreply.github.com>
2024-10-15 22:24:36 +0400

    add payment

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ import { cartActions } from "store/redux/cart/cartSlice"
 import AllProductsAdmin from "pages/AllProductsAdmin/AllProductsAdmin"
 import OrderForm from "pages/OrderForm/OrderForm"
 import AllOrdersAdmin from "pages/AllOrdersAdmin/AllOrdersAdmin"
+import PaymentSuccess from "pages/Payment/PaymentSuccess"
+import { QueryParamProvider } from "use-query-params"
+import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6"
 
 function App() {
   const { currentUser } = useAppSelector(userAuthSelectors.userAuthState)
@@ -38,21 +41,24 @@ function App() {
     <>
       <GlobalStyles />
       <Layout>
-        <Routes>
-          <Route path="/" element={<AllProducts />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/user-profile" element={<UserProfile />} />
-          {/* <Route path="/one-product-card" element={<OneProduct />} /> */}
-          <Route path="/:id" element={<OneProduct />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/add-product" element={<AddProductAdmin />} />
-          <Route path="/all-users" element={<AllUsers />} />
-          <Route path="/orders" element={<AllOrdersAdmin />} />
-          <Route path="/all-products-admin" element={<AllProductsAdmin />} />
-          <Route path="/order-form" element={<OrderForm />} />
-          <Route path="*" element={"error 404 - Page not found"} />
-        </Routes>
+        <QueryParamProvider adapter={ReactRouter6Adapter}>
+          <Routes>
+            <Route path="/" element={<AllProducts />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/user-profile" element={<UserProfile />} />
+            {/* <Route path="/one-product-card" element={<OneProduct />} /> */}
+            <Route path="/:id" element={<OneProduct />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/add-product" element={<AddProductAdmin />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/orders" element={<AllOrdersAdmin />} />
+            <Route path="/all-products-admin" element={<AllProductsAdmin />} />
+            <Route path="/order-form" element={<OrderForm />} />
+            <Route path="/payment/success" element={<PaymentSuccess />} />
+            <Route path="*" element={"error 404 - Page not found"} />
+          </Routes>
+        </QueryParamProvider>
       </Layout>
     </>
   )
