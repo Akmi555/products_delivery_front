af7c24e18859b38cf525eacbe312f8d7c3f6a400
“Elvira55872” <elli-x1996@mail.ru>
2024-10-16 12:33:01 +0200

    in the process of doing openable order

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,20 +1,95 @@
 import { getNormalDateAndTimeFromOrderObject } from "pages/AllOrdersAdmin/AllOrdersAdmin"
-import { DataContainer, OrderWrapper } from "./styles"
+import { DataContainer, OrderWrapper, OrderWrapper2 } from "./styles"
 import { orderObjDataProps } from "./types"
+// для раскрывающегося списка
+import Accordion from "@mui/material/Accordion"
+import AccordionActions from "@mui/material/AccordionActions"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
+import Button from "@mui/material/Button"
 
 function Order({ orderObjData }: orderObjDataProps) {
   return (
-    <OrderWrapper>
-      <DataContainer>
-        {getNormalDateAndTimeFromOrderObject(orderObjData.orderTime)}
-      </DataContainer>
-      <DataContainer> {orderObjData.address}</DataContainer>
-      <DataContainer>
-        {getNormalDateAndTimeFromOrderObject(orderObjData.deliveryTime)}
-      </DataContainer>
-      <DataContainer> {orderObjData.totalSum}</DataContainer>
-      <DataContainer> {orderObjData.orderStatus}</DataContainer>
-    </OrderWrapper>
+    <>
+      {/* <OrderWrapper>
+        <DataContainer>
+          {getNormalDateAndTimeFromOrderObject(orderObjData.orderTime)}
+        </DataContainer>
+        <DataContainer> {orderObjData.address}</DataContainer>
+        <DataContainer>
+          {getNormalDateAndTimeFromOrderObject(orderObjData.deliveryTime)}
+        </DataContainer>
+        <DataContainer> {orderObjData.totalSum}</DataContainer>
+        <DataContainer> {orderObjData.orderStatus}</DataContainer>
+      </OrderWrapper> */}
+
+
+
+
+
+
+
+
+
+
+
+
+      <OrderWrapper2>
+
+      <Accordion sx={{borderRadius: 50}}>
+          <AccordionSummary
+            // expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+          >
+
+        <DataContainer>
+          {getNormalDateAndTimeFromOrderObject(orderObjData.orderTime)}
+        </DataContainer>
+        <DataContainer> {orderObjData.address}</DataContainer>
+        <DataContainer>
+          {getNormalDateAndTimeFromOrderObject(orderObjData.deliveryTime)}
+        </DataContainer>
+        <DataContainer> {orderObjData.totalSum}</DataContainer>
+        <DataContainer> {orderObjData.orderStatus}</DataContainer>
+
+        </AccordionSummary>
+          <AccordionDetails>
+            тут продукты которые есть в заказе
+          </AccordionDetails>
+        </Accordion>
+
+
+      </OrderWrapper2>
+
+
+
+
+
+
+
+
+
+
+
+      {/* <div>
+        <Accordion>
+          <AccordionSummary 
+          // expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1-content"
+            id="panel1-header"
+            >
+
+            </AccordionSummary>
+          <AccordionDetails></AccordionDetails>
+          <AccordionActions>
+            <Button>Cancel</Button>
+            <Button>Agree</Button>
+          </AccordionActions>
+        </Accordion>
+      </div> */}
+    </>
   )
 }
 
--- src/components/Order/styles.ts
+++ src/components/Order/styles.ts
@@ -12,6 +12,18 @@ export const OrderWrapper = styled.div`
   padding: 20px;
 `
 
+export const OrderWrapper2 = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  gap: 20px;
+  /* width: 800px; */
+  align-items: center;
+  /* background-color: white; */
+  border-radius: 30px;
+  padding: 20px;
+`
+
 export const DataContainer = styled.div`
   min-width: 130px;
   max-width:  130px;
