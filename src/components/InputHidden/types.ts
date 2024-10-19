cbd528a5c59ffa8062235ab66381bd7fda758cba
“Elvira55872” <elli-x1996@mail.ru>
2024-10-19 17:46:28 +0200

    fixed any type in Hidden Input for unpoading photos

--- src/components/InputHidden/types.ts
+++ src/components/InputHidden/types.ts
@@ -1,9 +1,9 @@
-import { HTMLInputTypeAttribute, ChangeEvent } from "react"
+import { HTMLInputTypeAttribute, ChangeEvent, RefObject } from "react"
 
 export interface InputProps {
   type?: HTMLInputTypeAttribute
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void
   accept?: string
   //!  починить any
-  $ref?: any
+  $ref?: null | RefObject<HTMLInputElement> 
 }
